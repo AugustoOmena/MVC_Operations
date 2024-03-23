6c8015b17deb6d8554970ffbf5ff1fa8e958e767
Augusto Omena <augusto.omena.n@gmail.com>
2024-03-23 10:26:03 -0300

    Adicionar arquivos de projeto.

---
+++ MVC_Operations/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MVC_Operations.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public int Name { get; set; }
+        public int Email { get; set; }
+    }
+}
