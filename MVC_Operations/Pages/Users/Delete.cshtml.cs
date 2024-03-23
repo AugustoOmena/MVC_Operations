2e9157ca6eedecbc267963cd694d42216e487313
Augusto Omena <augusto.omena.n@gmail.com>
2024-03-23 11:29:15 -0300

    Adding User scalfold (CRUD).

---
+++ MVC_Operations/Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MVC_Operations.Data;
+using MVC_Operations.Models;
+
+namespace MVC_Operations.Pages.Users
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly MVC_Operations.Data.MVC_OperationsContext _context;
+
+        public DeleteModel(MVC_Operations.Data.MVC_OperationsContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                User = user;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FindAsync(id);
+            if (user != null)
+            {
+                User = user;
+                _context.User.Remove(User);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ MVC_Operations/Pages/Users/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MVC_Operations.Data;
+using MVC_Operations.Models;
+
+namespace MVC_Operations.Pages.Users
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MVC_Operations.Data.MVC_OperationsContext _context;
+
+        public DetailsModel(MVC_Operations.Data.MVC_OperationsContext context)
+        {
+            _context = context;
+        }
+
+        public User User { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await _context.User.FirstOrDefaultAsync(m => m.ID == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                User = user;
+            }
+            return Page();
+        }
+    }
+}
